c6a75b8ba98f5cd8204e29ddf239e7fc2de997e8
hanstimo <112049897+hanstimo@users.noreply.github.com>
2026-08-09 21:47:29 +0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,20 +2,20 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+        'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+        'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+        'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
 
-    'allowed_origins_patterns' => [],
+        'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
 
-    'allowed_headers' => ['*'],
+        'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+        'exposed_headers' => [],
 
-    'max_age' => 0,
+        'max_age' => 0,
 
-    'supports_credentials' => false,
+        'supports_credentials' => false,
 
-];
+    ];
