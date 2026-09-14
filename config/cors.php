2c025dff9227d6291bac3f90079b634d7861523d
hanstimo <112049897+hanstimo@users.noreply.github.com>
2026-09-14 17:41:42 +0700

    Lock CORS to the production frontend origin

--- config/cors.php
+++ config/cors.php
@@ -6,9 +6,9 @@ return [
 
         'allowed_methods' => ['*'],
 
-        'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+        'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://leciel-appointment.vercel.app'],
 
-        'allowed_origins_patterns' => ['#^https://.*\.vercel\.app$#'],
+        'allowed_origins_patterns' => [],
 
         'allowed_headers' => ['*'],
 
